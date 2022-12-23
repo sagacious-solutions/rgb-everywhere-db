dab320f82e34b15e736830717079078d7e4ddc36
matt macdonald <mksmacdonald@gmail.com>
2022-12-23 15:21:26 -0800

    enhanced commenting

--- db.py
+++ db.py
@@ -18,11 +18,28 @@ class Database:
         self.create_tables()
 
     def add_device(self, new_device: Dict[str, str]):
+        """Add a new Device to the database
+
+        Args:
+            new_device (Dict[str, str]): Device details to add to db
+        """
         new_db_entry = device.Device(**new_device)
         self.session.add(new_db_entry)
         self.session.commit()
 
-    def get_device(self, ip_address):
+    def get_device(self, ip_address: str) -> Device:
+        """Gets a specific device by IP address from the database
+
+        Args:
+            ip_address (str): IP Address of the new device
+
+        Raises:
+            ValueError: Thrown if we get multi results for an IP Address
+
+        Returns:
+            Device: The device you saught
+        """
+
         devices = []
         query = select(Device.__table__).where(
             Device.__table__.c.ip_address == ip_address
@@ -33,12 +50,20 @@ class Database:
         if len(devices) > 1:
             raise ValueError(
                 "Database query should have returned only one result per IP"
-                " Address"
+                " Address."
             )
 
         return devices[0]
 
     def update_device(self, device_to_update: Dict[str, str]):
+        """Updates an existing device in the database
+
+        Args:
+            device_to_update (Dict[str, str]): The device in the database to update
+
+        Raises:
+            ValueError: Raise if no device was provided to query database
+        """
         if not device_to_update:
             raise ValueError("No device was passed to update function.")
 
@@ -52,9 +77,16 @@ class Database:
         )
         self.session.execute(update_entry)
         self.session.commit()
-        return True
 
     def delete_device(self, device_to_delete: Dict[str, str]):
+        """Deletes a device from the provided dict from the database.
+
+        It will use the ip_address key to locate the item
+
+        Args:
+            device_to_delete (Dict[str, str]): Device to delete, must have a ip_address
+                key
+        """
         delete_entry = Device.__table__.delete().where(
             Device.__table__.c.ip_address == device_to_delete.get("ip_address")
         )
